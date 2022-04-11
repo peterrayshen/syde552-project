e1d228da05c055bf2c7fca52957018a9e752ac7f
Peter Ray Shen <peterrayshen@gmail.com>
2022-04-10 21:38:32 -0400

    update

--- learn_curve.py
+++ learn_curve.py
@@ -1,6 +1,7 @@
 import os
 import h5py
 import pandas as pd
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,27 +13,45 @@ import torch.nn as nn
 import torchvision
 from torch.utils import data
 import pickle
+import argparse
 
 from utils import SurrGradSpike, get_shd_dataset, sparse_data_generator_from_hdf5_spikes, sparse_data_generator_from_hdf5_spikes_2
 from collections import Counter
 from sklearn.model_selection import train_test_split
 from constants import xp_type_tau_constant, xp_type_tau_gauss, xp_type_tau_uniform, nb_inputs, nb_hidden, nb_outputs, time_step, nb_steps, max_time, batch_size, tau_mem_readout, tau_syn
 
-# uniform tau xp params
-uniform_lower_bounds = np.array([38, 35, 30, 20]) * 1e-3
-uniform_upper_bounds = np.array([42, 45, 50, 60]) * 1e-3
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
 
-# gaussian tau xp params
-gauss_mean = 40e-3
-gauss_std = 20e-3
-
-# constant tau xp params
-constant_tau = 40e-3
+xp_type = sys.argv[1]
 
 # general xp params
-xp_type = xp_type_tau_uniform
 nb_epochs = 2
 lr = 1e-3
+
+if xp_type == xp_type_tau_uniform:
+    xp_params = {
+        'uniform_lower_bounds': np.array([38, 35, 30, 20]) * 1e-3,
+        'uniform_upper_bounds':  np.array([42, 45, 50, 60]) * 1e-3,
+        'num_xps': 4,
+    }
+elif xp_type == xp_type_tau_gauss:
+    xp_params = {
+        'gauss_mean': np.array([40, 40, 40, 40]) * 1e-3,
+        'gauss_std':  np.array([2, 5, 10, 20]) * 1e-3,
+        'num_xps': 4,
+    }
+elif xp_type == xp_type_tau_constant:
+    xp_params = {
+        'constant_tau': np.array([20, 40, 60, 80]) * 1e-3,
+        'num_xps': 4,
+    }
+else:
+    raise ValueError('xp type not recognized')
 
 
 dtype = torch.float
@@ -64,25 +83,26 @@ beta_readout    = float(np.exp(-time_step/tau_mem_readout))
 
 xp_results = []
 
-assert uniform_lower_bounds.shape[0] == uniform_upper_bounds.shape[0]
-for i in range(len(uniform_lower_bounds)):
-
+for i in range(xp_params['num_xps']):
     if xp_type == xp_type_tau_uniform:
-        uniform_lower = uniform_lower_bounds[i]
-        uniform_upper = uniform_upper_bounds[i]
-        uniform_tau_np = np.tile(np.random.uniform(low=uniform_lower, high=uniform_upper, size=nb_hidden), (batch_size, 1))
-        uniform_beta_np = np.exp(-time_step/uniform_tau_np)
-        beta_distribution = torch.from_numpy(uniform_beta_np).float().to(device=device)
+        uniform_lower = xp_params['uniform_lower_bounds'][i]
+        uniform_upper = xp_params['uniform_upper_bounds'][i]
+        gen_tau_np = np.tile(np.random.uniform(low=uniform_lower, high=uniform_upper, size=nb_hidden), (batch_size, 1))
+        print(f"init done for lower bound {uniform_lower}, upper bound {uniform_upper}")
     elif xp_type == xp_type_tau_gauss:
-        gauss_tau_np = np.tile(np.random.normal(loc=gauss_mean, scale=gauss_std, size=(nb_hidden)), (batch_size, 1))
-        gauss_beta_np =  np.exp(-time_step/gauss_tau_np)
-        beta_distribution = torch.from_numpy(gauss_beta_np).float().to(device=device) 
+        gauss_mean = xp_params['gauss_mean'][i]
+        gauss_std = xp_params['gauss_std'][i]
+        gen_tau_np = np.tile(np.random.normal(loc=gauss_mean, scale=gauss_std, size=(nb_hidden)), (batch_size, 1))
+        print(f"init done for mean {gauss_mean}, stddev {gauss_std}")
     elif xp_type == xp_type_tau_constant:
-        constant_tau_np = np.ones((batch_size,nb_hidden)) * constant_tau
-        constant_beta_np = np.exp(-time_step/constant_tau_np)
-        beta_distribution = torch.from_numpy(constant_beta_np).float().to(device=device) 
+        constant_tau = xp_params['constant_tau'][i]
+        gen_tau_np = np.ones((batch_size,nb_hidden)) * constant_tau
+        print(f"init done for constant tau {constant_tau}")
     else:
         raise ValueError('xp type not recognized')
+
+    beta_np = np.exp(-time_step/gen_tau_np)
+    beta_torch = torch.from_numpy(beta_np).float().to(device=device)
 
     weight_scale = 0.2
 
@@ -94,8 +114,6 @@ for i in range(len(uniform_lower_bounds)):
 
     v1 = torch.empty((nb_hidden, nb_hidden), device=device, dtype=dtype, requires_grad=True)
     torch.nn.init.normal_(v1, mean=0.0, std=weight_scale/np.sqrt(nb_hidden))
-
-    print(f"init done for lower bound {uniform_lower}, upper bound {uniform_upper}")
 
     spike_fn  = SurrGradSpike.apply
 
@@ -116,7 +134,7 @@ for i in range(len(uniform_lower_bounds)):
             rst = out.detach() # We do not want to backprop through the reset
 
             new_syn = alpha*syn +h1
-            new_mem =(beta_distribution*mem +syn)*(1.0-rst)
+            new_mem =(beta_torch*mem +syn)*(1.0-rst)
 
             mem_rec.append(mem)
             spk_rec.append(out)
@@ -145,6 +163,32 @@ for i in range(len(uniform_lower_bounds)):
         other_recs = [mem_rec, spk_rec]
         return out_rec, other_recs
 
+    def compute_classification_accuracy(x_data, y_data):
+        """ Computes classification accuracy on supplied data in batches. """
+        accs = []
+        for x_local, y_local in sparse_data_generator_from_hdf5_spikes(x_data, y_data, batch_size, nb_steps, nb_inputs, max_time, device, shuffle=False):
+            output,_ = run_snn(x_local.to_dense())
+            m,_= torch.max(output,1) # max over time
+            _,am=torch.max(m,1)      # argmax over output units
+            tmp = np.mean((y_local==am).detach().cpu().numpy()) # compare to labels
+            accs.append(tmp)
+        return np.mean(accs)
+
+    def compute_classification_accuracy_2(x_data, y_data, device):
+        """ Computes classification accuracy on supplied data in batches. """
+        accs = []
+        for x_local, y_local in sparse_data_generator_from_hdf5_spikes_2(x_data, y_data, batch_size, nb_steps, nb_inputs, max_time, shuffle=False, device=device):
+            output,_ = run_snn(x_local.to_dense())
+            m,_= torch.max(output,1) # max over time
+            _,am=torch.max(m,1)      # argmax over output units
+            tmp = np.mean((y_local==am).detach().cpu().numpy()) # compare to labels
+            accs.append(tmp)
+        return np.mean(accs)
+
+    def get_mini_batch(x_data, y_data, shuffle=False):
+        for ret in sparse_data_generator_from_hdf5_spikes(x_data, y_data, batch_size, nb_steps, nb_inputs, max_time, device, shuffle=shuffle):
+            return ret 
+
     def train_with_validation(
         x_train_train, 
         y_train_train,
@@ -163,6 +207,9 @@ for i in range(len(uniform_lower_bounds)):
         loss_train_reg = []
         loss_valid_supervised = []
         loss_valid_reg = []
+        acc_train = []
+        acc_valid = []
+
         for e in range(nb_epochs):
             # train
             local_loss_train_supervised = []
@@ -191,7 +238,9 @@ for i in range(len(uniform_lower_bounds)):
             mean_valid_loss = np.mean(local_loss_train_reg)
             loss_train_supervised.append(mean_supervised_loss)
             loss_train_reg.append(mean_valid_loss)
-            print("Epoch %i: train loss=%.5f"%(e+1,mean_supervised_loss + mean_valid_loss))
+            local_acc_train = compute_classification_accuracy_2(x_train_train, y_train_train, device)
+            acc_train.append(local_acc_train)
+            print("Epoch %i: train loss=%.5f, train acc=%.5f"%(e+1,mean_supervised_loss + mean_valid_loss, local_acc_train))
 
             # validation
             local_loss_valid_supervised = []
@@ -214,41 +263,30 @@ for i in range(len(uniform_lower_bounds)):
             mean_valid_loss = np.mean(local_loss_valid_reg)
             loss_valid_supervised.append(mean_supervised_loss)
             loss_valid_reg.append(mean_valid_loss)
-            print("Epoch %i: validation loss=%.5f"%(e+1,mean_supervised_loss + mean_valid_loss))
+            local_acc_valid = compute_classification_accuracy_2(x_train_valid, y_train_valid, device)
+            acc_valid.append(local_acc_valid)
+            print("Epoch %i: validation loss=%.5f, validation acc=%.5f"%(e+1,mean_supervised_loss + mean_valid_loss, local_acc_valid))
         
-        return loss_train_supervised, loss_valid_supervised, loss_train_reg, loss_valid_reg
+        return loss_train_supervised, loss_valid_supervised, loss_train_reg, loss_valid_reg, acc_train, acc_valid
 
-    def compute_classification_accuracy(x_data, y_data):
-        """ Computes classification accuracy on supplied data in batches. """
-        accs = []
-        for x_local, y_local in sparse_data_generator_from_hdf5_spikes(x_data, y_data, batch_size, nb_steps, nb_inputs, max_time, device, shuffle=False):
-            output,_ = run_snn(x_local.to_dense())
-            m,_= torch.max(output,1) # max over time
-            _,am=torch.max(m,1)      # argmax over output units
-            tmp = np.mean((y_local==am).detach().cpu().numpy()) # compare to labels
-            accs.append(tmp)
-        return np.mean(accs)
-
-    def get_mini_batch(x_data, y_data, shuffle=False):
-        for ret in sparse_data_generator_from_hdf5_spikes(x_data, y_data, batch_size, nb_steps, nb_inputs, max_time, device, shuffle=shuffle):
-            return ret 
-
-    loss_supervised_train, loss_supervised_valid, loss_reg_train, loss_reg_valid = train_with_validation(x_train_train, y_train_train, x_train_valid, y_train_valid, lr=lr, nb_epochs=nb_epochs)
+    loss_supervised_train, loss_supervised_valid, loss_reg_train, loss_reg_valid, acc_train, acc_valid = train_with_validation(x_train_train, y_train_train, x_train_valid, y_train_valid, lr=lr, nb_epochs=nb_epochs)
 
     x_batch, y_batch = get_mini_batch(x_test, y_test)
     output, other_recordings = run_snn(x_batch.to_dense())
     mem_rec, spk_rec = other_recordings
+    acc_test = compute_classification_accuracy(x_test, y_test)
 
 
-    xp_results.append({
+    xp_res = {
         'w1': w1.cpu().detach().numpy(),
         'w2': w2.cpu().detach().numpy(),
         'v1': v1.cpu().detach().numpy(),
         'weight_scale': weight_scale,
-        'uniform_lower': uniform_lower,
-        'uniform_upper': uniform_upper,
-        'gen_distribution_tau': uniform_tau_np,
-        'gen_distribution_beta': uniform_beta_np,
+        'xp_type': xp_type,
+        'xp_params': xp_params,
+        'i': i,
+        'gen_distribution_tau': gen_tau_np,
+        'gen_distribution_beta': beta_np,
         'alpha': alpha,
         'beta_readout': beta_readout,
         'nb_inputs': nb_inputs,
@@ -265,12 +303,16 @@ for i in range(len(uniform_lower_bounds)):
         'loss_supervised_valid': loss_supervised_valid,
         'loss_reg_train': loss_reg_train,
         'loss_reg_valid': loss_reg_valid,
-        'output': output,
-        'mem_rec': mem_rec,
-        'spk_rec': spk_rec,
-    })
+        'acc_test': acc_test,
+        'acc_train': acc_train,
+        'acc_valid': acc_valid,
+        'output': output.cpu().detach().numpy(),
+        'mem_rec': mem_rec.cpu().detach().numpy(),
+        'spk_rec': spk_rec.cpu().detach().numpy(),
+    }
+    xp_results.append(xp_res)
 
-with open('results/learn_curve_uniform_tau.pkl', 'wb') as f:
+with open(f'results/learn_curve_{xp_type}.pkl', 'wb') as f:
     pickle.dump(xp_results, f)
 
 
